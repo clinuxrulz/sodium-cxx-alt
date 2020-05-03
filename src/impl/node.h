7b23155dd946b891517136dd83ffe06c8e4ee8cf
Clinton Selke <clinuxrulz@gmail.com>
2020-05-03 12:47:18 +1000

    wip sodium_ctx

--- src/impl/node.h
+++ src/impl/node.h
@@ -161,10 +161,6 @@ public:
     virtual std::unique_ptr<IsWeakNode> downgrade();
 } Node;
 
-GcNode IsNode::gc_node() {
-    return this->node().gc_node;
-}
-
 typedef struct NodeData {
     bool visited;
     bool changed;
@@ -213,26 +209,9 @@ typedef struct WeakNode: public IsWeakNode {
     }
 } WeakNode;
 
-std::unique_ptr<IsWeakNode> Node::downgrade() {
-    WeakNode* node = new WeakNode(this->data, this->gc_node, this->sodium_ctx);
-    return std::unique_ptr<IsWeakNode>((IsWeakNode*)node);
-}
+std::vector<std::unique_ptr<IsNode>> box_clone_vec_is_node(std::vector<std::unique_ptr<IsNode>>& xs);
 
-std::vector<std::unique_ptr<IsNode>> box_clone_vec_is_node(std::vector<std::unique_ptr<IsNode>>& xs) {
-    std::vector<std::unique_ptr<IsNode>> result;
-    for (auto x = xs.begin(); x != xs.end(); ++x) {
-        result.push_back((*x)->box_clone());
-    }
-    return result;
-}
-
-std::vector<std::unique_ptr<IsWeakNode>> box_clone_vec_is_weak_node(std::vector<std::unique_ptr<IsWeakNode>>& xs) {
-    std::vector<std::unique_ptr<IsWeakNode>> result;
-    for (auto x = xs.begin(); x != xs.end(); ++x) {
-        result.push_back((*x)->box_clone());
-    }
-    return result;
-}
+std::vector<std::unique_ptr<IsWeakNode>> box_clone_vec_is_weak_node(std::vector<std::unique_ptr<IsWeakNode>>& xs);
 
 }
 
