5e133565e915db87c0de43594613cff2c62033c9
Clinton Selke <clinuxrulz@gmail.com>
2020-05-07 16:22:07 +1000

    wip lambda

---
+++ sodium/impl/lambda.h
@@ -0,0 +1,38 @@
+#ifndef __SODIUM_IMPL_LAMBDA_H__
+#define __SODIUM_IMPL_LAMBDA_H__
+
+#include <vector>
+#include "sodium/impl/dep.h"
+
+namespace sodium {
+
+namespace impl {
+
+template<typename FN>
+class Lambda {
+public:
+    FN fn;
+    std::vector<Dep> deps;
+
+    // Lambda(FN fn, std::vector<Dep> deps): fn(fn), deps(deps) {}
+};
+
+template<typename LAMBDA>
+struct GetDeps {
+    std::vector<Dep> operator() const {
+        return std::vector<Dep>();
+    }
+};
+
+template<typename FN>
+struct GetDeps<Lambda<FN>> {
+    std::vector<Dep> operator() const {
+        return this->deps;
+    }
+};
+
+}
+
+}
+
+#endif // __SODIUM_IMPL_LAMBDA_H__
