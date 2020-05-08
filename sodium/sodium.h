e097a96cba3351a455b17409594c8681e56b6eca
Clinton Selke <clinuxrulz@gmail.com>
2020-05-08 17:07:40 +1000

    api notes

--- sodium/sodium.h
+++ sodium/sodium.h
@@ -10,7 +10,10 @@
 
 namespace sodium {
 
-
+    // TODO: Create user API via wrappers of all the impl folder stuff.
+    //
+    // TODO: Create global SodiumCtx to be used via the wrappers, so
+    //       end user does not need to create and pass around a SodiumCtx.
 
 }
 
