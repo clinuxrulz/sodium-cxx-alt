b10fdfc91e2ce4fe661a520a40a99e71111e9575
Clinton Selke <clinuxrulz@gmail.com>
2020-05-14 13:35:36 +1000

    wip exposed transaction api

---
+++ sodium/transaction.h
@@ -0,0 +1,42 @@
+#include "sodium/impl/sodium_ctx.h"
+
+namespace sodium {
+
+    namespace impl {
+        extern SodiumCtx sodium_ctx;
+    }
+
+    class transaction
+    {
+        private:
+            sodium::impl::Transaction impl_;
+            // Disallow copying
+            transaction(const transaction&): impl_(impl::sodium_ctx) {}
+            // Disallow copying
+            transaction& operator = (const transaction&) { return *this; };
+        public:
+            transaction(): impl_(impl::sodium_ctx) {}
+            /*!
+             * The destructor will close the transaction, so normally close() isn't needed.
+             * But, in some cases you might want to close it earlier, and close() will do this for you.
+             */
+            inline void close() { impl_.close(); }
+
+            /* TODO: ???
+            void prioritized(impl::prioritized_entry* e)
+            {
+                impl()->prioritized(e);
+            }*/
+
+            void post(std::function<void()> f) {
+                impl::sodium_ctx.post(f);
+            }
+
+            // TODO: Implement this
+            /*
+            static void on_start(std::function<void()> f) {
+                transaction trans;
+                trans.impl()->part->on_start(std::move(f));
+            }*/
+    };    
+}
